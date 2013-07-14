88534858102f18e5f972313503f2ffcd05ac7a10
HanhDZ <huyhanhdo93@gmail.com>
2013-07-14 18:37:47 +0700

    ok

--- Sanuce/Sanuce/FormMoreFloors.Designer.cs
+++ Sanuce/Sanuce/FormMoreFloors.Designer.cs
@@ -93,7 +93,7 @@
             // 
             // FormMoreFloors
             // 
-            this.AcceptButton = this.button2;
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(347, 147);
